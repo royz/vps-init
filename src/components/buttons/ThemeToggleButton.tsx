0a7d8227c229b949e8cf7981835e6635d87adf3f
royz <rajorshi.me@gmail.com>
2026-04-02 00:04:58 +0530

    new component

---
+++ src/components/buttons/ThemeToggleButton.tsx
@@ -0,0 +1,19 @@
+import { useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { Moon, Sun } from "lucide-react";
+import { IconButton } from "../IconButton";
+
+export function ThemeToggleButton() {
+  const { setColorScheme, colorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+
+  return (
+    <IconButton
+      variant="subtle"
+      color="gray"
+      icon={colorScheme === "dark" ? Sun : Moon}
+      onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+      description={colorScheme === "dark" ? "Turn on light mode" : "Turn on dark mode"}
+      hotkey="Mod+`"
+    />
+  )
+}
