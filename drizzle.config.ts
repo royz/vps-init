e16d9beeaa86e8e114b64975a875d2633c612d5b
royz <rajorshi.me@gmail.com>
2026-04-01 20:21:46 +0530

    delete

--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "sqlite",
-  schema: "./src/db/schema.ts",
-  out: './drizzle',
-  casing: "snake_case",
-});
