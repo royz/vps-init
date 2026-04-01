13bc337d99538c7959f19c0d2af40e733e83b608
royz <rajorshi.me@gmail.com>
2026-04-01 23:33:27 +0530

    fix: toolip when only hotkey is provided

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,5 +1,5 @@
-import { ActionIcon, type ActionIconProps, Group, Kbd, Text, Tooltip, type TooltipProps } from "@mantine/core";
-import { formatForDisplay, type Hotkey, useHotkey } from "@tanstack/react-hotkeys"
+import { ActionIcon, type ActionIconProps, Group, Text, Tooltip, type TooltipProps } from "@mantine/core";
+import { type Hotkey, useHotkey } from "@tanstack/react-hotkeys";
 import type { LucideIcon } from "lucide-react";
 import { useMemo } from "react";
 import { DisplayHotkey } from "./DisplayHotkey";
@@ -23,7 +23,6 @@ export function IconButton({ icon, onClick, hotkey, description, ...actionIconPr
 
   const tooltipProps = useMemo((): TooltipProps => {
     if (description && hotkey) {
-      console.log(description, hotkey)
       return {
         label: (
           <Group>
@@ -35,7 +34,7 @@ export function IconButton({ icon, onClick, hotkey, description, ...actionIconPr
     }
 
     if (description && !hotkey) return { label: description };
-    if (!description && hotkey) return { label: formatForDisplay(hotkey) };
+    if (!description && hotkey) return { label: <DisplayHotkey hotkey={hotkey} /> };
 
     return { label: "", disabled: true };
   }, [description, hotkey]);
